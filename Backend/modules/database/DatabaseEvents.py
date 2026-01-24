9995a1d304802d2b4d25969c6c5f470af603ef39
Kernel <kernelxdev@gmail.com>
2026-01-24 18:37:09 +0100

    add user tokens

--- Backend/modules/database/DatabaseEvents.py
+++ Backend/modules/database/DatabaseEvents.py
@@ -1,4 +1,5 @@
 from modules.database import Database
+import secrets
 
 def add_user(username, hashed_passwd, db: Database.FreecordDB) -> tuple[bool, str]:
     if not db:
@@ -7,6 +8,10 @@ def add_user(username, hashed_passwd, db: Database.FreecordDB) -> tuple[bool, st
     if db().exists('users', {'username': username}):
         return False, "User already exists"
     
-    db().insert('users', {'username': username, 'hashed_passwd': hashed_passwd})
+    try:
+        user_token = f"FCT_{secrets.token_urlsafe(84)}"
+        db().insert('users', {'username': username, 'hashed_passwd': hashed_passwd, 'user_token': user_token})
+    except Exception as e:
+        return False, f"Failed to add user: {e}"
     
     return True, "User added successfully"
