527ce856f2bd168b6a3c46cd279d60887ad20877
Kernel <kernelxdev@gmail.com>
2026-01-24 19:23:46 +0100

    login (not finished)
    not working yet

--- Backend/modules/ServerClasses.py
+++ Backend/modules/ServerClasses.py
@@ -20,7 +20,6 @@ class MessageServerHandler(http.server.SimpleHTTPRequestHandler):
                 passwdhash = data.get('passwdhash')
                 
                 if name and passwdhash:
-                    
                     success, message = Events.create_account(name, passwdhash, Database.FreecordDB)
                     if not success:
                         self.send_error(400, message)
@@ -34,6 +33,47 @@ class MessageServerHandler(http.server.SimpleHTTPRequestHandler):
                     self.send_error(400, "Missing name or password")
             except json.JSONDecodeError:
                 self.send_error(400, "Invalid JSON")
+            except Exception as e:
+                self.send_error(400, e)
+        elif self.path == '/login':
+            content_length = int(self.headers.get('Content-Length', 0))
+            body = self.rfile.read(content_length)
+
+            try:
+                data = json.loads(body.decode('utf-8'))
+                name = data.get('name')
+                passwdhash = data.get('passwdhash')
+
+                if name and passwdhash:
+                    user_list = Database.FreecordDB().select("users", {"name": name})
+                    
+                    if not user_list:
+                        self.send_error(404, "User doesn't exist")
+                        return
+
+                    user = user_list[0]
+                    if user['passwdhash'] == passwdhash:
+                        response_data = {
+                            "status": "success",
+                            "message": "Logged in successfully",
+                            "user_token": user['user_token']
+                        }
+                        response_bytes = json.dumps(response_data).encode('utf-8')
+
+                        self.send_response(200)
+                        self.send_header('Content-type', 'application/json')
+                        self.send_header('Content-Length', len(response_bytes))
+                        self.end_headers()
+                        self.wfile.write(response_bytes)
+                    else:
+                        self.send_error(401, "Invalid password")
+                else:
+                    self.send_error(400, "Missing name or password")
+
+            except json.JSONDecodeError:
+                self.send_error(400, "Invalid JSON")
+            except Exception as e:
+                self.send_error(500, str(e))
         else:
             self.send_error(404, "Not found")
 
